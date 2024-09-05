c9ad9020a13a42655b601ce24759b704ac3e3791
Bobur Yusupov <bobur.yu06@mail.ru>
2024-09-05 21:52:49 +0200

    Created hello world project

---
+++ basic-syntax/hello-world/main.cpp
@@ -0,0 +1,12 @@
+// The first C++ project "Hello, World!"
+
+#include <iostream> // Import iostream library
+
+using namespace std;
+
+// main() function where execution of code starts
+int main() {
+    cout << "Hello, World!"; // Output "Hello, World!"
+
+    return 0; 
+}
