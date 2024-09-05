38ab59af43baa928ee5c1d05103a2e9a16f13044
Bobur Yusupov <bobur.yu06@mail.ru>
2024-09-05 22:18:39 +0200

    header files

---
+++ basic-syntax/headers/adding.h
@@ -0,0 +1,3 @@
+int adding(int a, int b) {
+    return a + b;
+}
---
+++ basic-syntax/headers/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include "adding.h"
+
+int main() {
+    int a = 20;
+    int b = 30;
+
+    std::cout << adding(a, b);
+
+    return 0;    
+
+}
