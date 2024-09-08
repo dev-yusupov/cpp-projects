64198b9a4809cf392817984dfc2690b82de201fb
Bobur Yusupov <bobur.yu06@mail.ru>
2024-09-08 22:32:55 +0200

    template

---
+++ oop/simple-class-template.cpp
@@ -0,0 +1,10 @@
+class ClassName {
+    protected:
+        // Protected members of class
+    
+    private:
+        // Private members of the class
+
+    public:
+        // Public members of the class
+};
