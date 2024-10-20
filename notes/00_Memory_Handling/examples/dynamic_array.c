8aec7a6a175690f3c8ff62f256f5c035e8fe4919
Bobur Yusupov <bobur.yu06@mail.ru>
2024-10-20 15:09:26 +0200

    changed some lines

--- notes/00_Memory_Handling/examples/dynamic_array.c
+++ notes/00_Memory_Handling/examples/dynamic_array.c
@@ -5,10 +5,11 @@ int main() {
     int n;
 
     printf("Enter the length of array: ");
-    scanf("%d", &n);
+    scanf("%d", &n); // User defines the length of the array
 
-    int *arr = (int *)malloc(n * sizeof(int));
+    int *arr = (int *)malloc(n * sizeof(int)); // Manually allocate array for n integers
 
+    // If memory allocation fails it will raise an error.
     if (arr == NULL) {
         printf("Memory allocation failed");
         return 1;
