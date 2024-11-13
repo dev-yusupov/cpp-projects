ca49115af92b7bfbdd085ac7157808d522eadd63
Bobur Yusupov <bobur.yu06@mail.ru>
2024-11-13 21:03:09 +0100

    header, object files and linking

---
+++ basic-syntax/compilation/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "math_utils.h"
+
+int main() {
+    printf("Addition of 5 and 4 is %d\n", add(5, 4));
+    printf("Subtraction of 5 and 4 is %d\n", subtract(5, 4));
+
+    return 0;
+}
